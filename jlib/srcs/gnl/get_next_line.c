4eaa0309b4f9bd9ede6a59806e1814839fac581b
Jujupik <jujupik@hotmail.fr>
2020-06-04 13:05:13 +0200

    gnl2

--- jlib/srcs/gnl/get_next_line.c
+++ jlib/srcs/gnl/get_next_line.c
@@ -12,16 +12,26 @@
 
 #include "jlib.h"
 
-char	*g_saved[MAX_FD];
+char* g_saved[MAX_FD];
+
+int			open_fd(char* path, int mode)
+{
+	int fd = _open(path, mode);
+	if (g_saved[fd] != NULL)
+		free(g_saved[fd]);
+	g_saved[fd] = ft_strnew(0);
+	return (fd);
+}
 
 void			close_fd(int fd)
 {
 	if (g_saved[fd] != NULL)
 		free(g_saved[fd]);
 	g_saved[fd] = NULL;
+	_close(fd);
 }
 
-static int		get_next_line_result(char **line, char **saved)
+static int		get_next_line_result(char** line, char** saved)
 {
 	if (*line != NULL)
 		free(*line);
@@ -34,41 +44,25 @@ static int		get_next_line_result(char **line, char **saved)
 	return (ft_strlen(*line) + ft_strlen(*saved));
 }
 
-static int		handle_quit_get_next_line(char **saved_line, char **line)
-{
-	if (*saved_line != NULL)
-		free(*saved_line);
-	*saved_line = NULL;
-	if (*line != NULL)
-		free(*line);
-	*line = NULL;
-	return (0);
-}
-
-int				get_next_line(int fd, char **line)
+int				get_next_line(int fd, char** line)
 {
 	int			result;
-	char		*tmp;
+	char* tmp;
 	char		buff[BUFF_SIZE + 1];
 
 	if (fd == 1 || fd == 2 || fd >= MAX_FD)
 		return (-1);
 	result = 1;
-	while (ft_strcchr(g_saved[fd], '\n') <= 0)
+	while (ft_strcchr(g_saved[fd], '\n') == 0 && result > 0)
 	{
-		result = read(fd, buff, BUFF_SIZE);
-		if (result == 0 || (result == 1 && buff[0] == '\n'))
-		{
-			free(g_saved[fd]);
-			g_saved[fd] = NULL;
-			break ;
-		}
+		result = _read(fd, buff, BUFF_SIZE);
 		buff[result] = '\0';
-		if (is_only_compose(buff, '\0') == TRUE)
-			return (handle_quit_get_next_line(&(g_saved[fd]), line));
-		tmp = g_saved[fd];
-		g_saved[fd] = ft_strjoin(tmp, buff);
-		free(tmp);
+		if (result > 0)
+		{
+			tmp = g_saved[fd];
+			g_saved[fd] = ft_strjoin(tmp, buff);
+			free(tmp);
+		}
 	}
 	return (get_next_line_result(line, &(g_saved[fd])));
 }
